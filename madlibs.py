4157fe75a56a3beaf81ab33a63f77df4da9fd5b6
aldershko <95867250+aldershko@users.noreply.github.com>
2023-02-20 12:53:12 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+
+verb1 = input("Verb:")
+verb2 = input("verb:")
+famous_person = input("famous_person:")
+verb3 = input("Verb:")
+
+
+
+madlib = f"Programming is a fun activity like {verb1}.It makes you wanna do things like {verb2}.When you are coding you feel like you are {famous_person} {verb3} to save the world!."
+print(madlib)
